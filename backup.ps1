f4ce5b20a1de166ab97d6e412e6dba233ee300a0
aman-w-kumar <45878098+aman-w-kumar@users.noreply.github.com>
2018-12-31 10:52:00 +0100

    backupv2.ps1
    new updates

--- backup.ps1
+++ backup.ps1
@@ -21,23 +21,18 @@ begin {
            
         #$lastdate = $date.adddays(-7)
         $lastdate = $date.addhours(-0)
-       # $logfile = https://vdatadomain.file.core.windows.net/logshare
+     
 
 try {
-    $servicePrincipalConnection = Get-AutomationConnection -Name 'AzureRunAsConnection'  
-    "Logging in to Azure..."
+     "Logging in to Azure..."
      $credentials = Get-AutomationPSCredential -Name 'AzureCredential'
      add-AzureRmAccount -Credential $credentials
     
-    #Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -verbose 
      $storageAcct = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -ErrorAction Stop
      $storagecontext = ($storageAcct).Context
      $share = Get-AzureStorageShare -Context $storagecontext -Name $filesharename -ErrorAction Stop
      $snapshot = $share.Snapshot()
-     #$snapshotname = $snapshot.name + $timestamp
-     #Get-AzureStorageFile -Share $snapshot.Name
      $listofsnapshots = Get-AzureStorageShare -Context $storageContext | Where-Object { $_.Name -eq $filesharename -and $_.IsSnapshot -eq $true }
-     #$listofsnapshots | Select-Object Name, SnapShotTime,IsSnapshot
      ###################count the snapshots age > 7 days #################  should be -lt operator
      $oldsnapshots = @($listofsnapshots | Where-Object { $_.SnapshotTime -lt $lastdate})
      if ($oldsnapshots -ne 'null')
@@ -48,19 +43,16 @@ try {
           "----------------------------------------------------------------"
           $removesnapshots = Remove-AzureStorageShare -Share $oldsnapshot -verbose -WhatIf
           
-              } #for each
+              } 
        Write-Verbose "snapshot removed $removesnapshots "
-     }#if
-} #try
+     }
+} 
 
 catch {
     $ErrorMessage = $_.Exception.Message
     "[$timestamp] [Error] at line $($_.InvocationInfo.ScriptLineNumber): $ErrorMessage" 
-
- }
- 
- 
 }
+ }
 end {
          "---------------------------------------------------------------------------------------------------"
          "[$timestamp] Function Ended "
